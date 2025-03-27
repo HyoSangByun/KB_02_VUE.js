1f693a44a2a7337fef82ade61f87b1daac3f370b
HyoSangByun <billy04100410@gmail.com>
2025-03-27 16:17:49 +0900

    20250327 심화

---
+++ 20250327/심화/todolist-app/src/components/TodoCount.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="row mb-3">
+    <div class="col-md text-center">
+      전체 todo 개수 : <span>{{ todolist.length }}</span>
+    </div>
+    <div class="col-md text-center">
+      <!-- 완료 todo 개수 : <span :class="completedCount && completed">{{completedCount}}</span> -->
+      완료 todo 개수 :
+      <span :class="{ completed: completedCount > 0 }">{{
+        completedCount
+      }}</span>
+    </div>
+    <div class="col-md text-center">
+      미완료 todo 개수 :
+      <span :class="{ notCompleted: todolist.length - completedCount > 0 }">{{
+        todolist.length - completedCount
+      }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {} from 'vue';
+
+const props = defineProps({
+  completedCount: { type: Number, required: true },
+  todolist: { type: Array, required: true },
+});
+// export default {
+//   name: 'TodoCount',
+
+//   props: {
+//     completedCount: { type: Number, required: true },
+//     todolist: { type: Array, required: true },
+//   },
+// };
+</script>
