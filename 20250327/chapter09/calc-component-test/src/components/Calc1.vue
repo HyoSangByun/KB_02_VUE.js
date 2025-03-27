449c8074bf774574700c7e2f4656b99658edc764
HyoSangByun <billy04100410@gmail.com>
2025-03-27 15:07:25 +0900

    20250327 기본

---
+++ 20250327/chapter09/calc-component-test/src/components/Calc1.vue
@@ -0,0 +1,49 @@
+<!-- Options API 적용 -->
+<template>
+  <div>
+    <h3>Options API</h3>
+    <input
+      type="text"
+      placeholder="입력하면 대문자로 변환"
+      :value="name"
+      @input="changeName($event.target.value)"
+    />
+  </div>
+
+  <div>
+    X : <input type="text" v-model.number="x" /><br />
+    Y : <input type="text" v-model.number="y" /><br />
+
+    <button @click="calclateSum(x, y)">계산하기</button>
+    <p>X + Y = {{ sum1 }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calc1', //컴포넌트 이름 지정
+  data() {
+    return {
+      // name 관련 기능
+      name: '',
+
+      //  계산 관련 기능
+      x: 10,
+      y: 20,
+      sum1: 30,
+    };
+  },
+
+  methods: {
+    // name 관련 메서드
+    changeName(inputName) {
+      this.name = inputName.toUpperCase(); //대문자 변환
+    },
+
+    //계산 관련 메서드
+    calclateSum(x, y) {
+      this.sum1 = x + y;
+    },
+  },
+};
+</script>
---
+++ 20250327/chapter09/calc-component-test/src/components/Calc2.vue
@@ -0,0 +1,48 @@
+<!-- Options API 적용 -->
+<template>
+  <div>
+    <h3>Composition API</h3>
+    <input
+      type="text"
+      placeholder="입력하면 대문자로 변환"
+      :value="name"
+      @input="changeName($event.target.value)"
+    />
+  </div>
+
+  <div>
+    X : <input type="text" v-model.number="x" /><br />
+    Y : <input type="text" v-model.number="y" /><br />
+
+    <button @click="calclateSum(x, y)">계산하기</button>
+    <p>X + Y = {{ sum2 }}</p>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'Calc2', //컴포넌트 이름 지정
+
+  setup() {
+    const name = ref(''); // data() 대체
+
+    const changeName = (inputName) => {
+      name.value = inputName.toUpperCase();
+    };
+
+    // -------------------------------------------------
+    const x = ref(10);
+    const y = ref(20);
+    const sum2 = ref(30);
+
+    const calclateSum = (x, y) => {
+      sum2.value = x + y;
+    };
+
+    //setup() 리턴 구문에 작성 -> <template>에서 사용 가능
+    return { name, changeName, x, y, sum2, calclateSum };
+  },
+};
+</script>
